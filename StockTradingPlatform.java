111443f0113ec6e6fb849ab17f1d39bd4143646d
shikha-code0 <sy5953077@gmail.com>
2026-06-08 15:50:36 +0530

    Add files via upload
    second project

---
+++ StockTradingPlatform.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Stock{
+    String symbol;
+    double price;
+    Stock(String symbol, double price){
+        this.symbol=symbol;
+        this.price=price;
+    }
+}
+class User{
+double balance;
+HashMap<String,Integer> portfolio=new;
+HashMap<>();
+public class StockTradingPlatform {
+}
+}
